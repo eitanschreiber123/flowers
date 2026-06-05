6da96f57c3ad1e64b1f4fdefacf2cb643ca14664
eitanschreiber123 <eitanschreiber97@gmail.com>
2026-06-05 09:26:59 -0500

    middle section

--- src/components/footer.js
+++ src/components/footer.js
@@ -20,25 +20,33 @@ export default function Footer() {
         <div className="space-y-5" style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
           <h2 className="text-[1.2em] font-semibold">Florinda Floreria</h2>
 
-          <div className="flex items-start gap-3 text-[1.2em] text-gray-700">
+          <div className="flex items-start gap-3 text-[1.2em] text-gray-700" style={{display:'flex',alignItems:'center'}}>
             <svg width="20" height="20" viewBox="0 0 384 512" fill="currentColor">
               <path d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0zM192 272c44.183 0 80-35.817 80-80s-35.817-80-80-80-80 35.817-80 80 35.817 80 80 80z" />
             </svg>
-            <p>Hurtado entre Tungurahua y Lizardo García</p>
+            <p>Guayaquil, Ecuador</p>
           </div>
 
-          <div className="flex items-start gap-3 text-[1.2em] text-gray-700">
+          <div className="flex items-start gap-3 text-[1.2em] text-gray-700"style={{display:'flex',alignItems:'center'}}>
             <svg width="20" height="20" viewBox="0 0 512 512" fill="currentColor">
               <path d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm0 448c-110.5 0-200-89.5-200-200S145.5 56 256 56s200 89.5 200 200-89.5 200-200 200zm61.8-104.4l-84.9-61.7c-3.1-2.3-4.9-5.9-4.9-9.7V116c0-6.6 5.4-12 12-12h32c6.6 0 12 5.4 12 12v141.7l66.8 48.6c5.4 3.9 6.5 11.4 2.6 16.8L334.6 349c-3.9 5.3-11.4 6.5-16.8 2.6z" />
             </svg>
-            <p>Lun - Sab de 8:30am - 6:00pm</p>
+            <p>Siempre</p>
           </div>
 
-          <div className="flex items-start gap-3 text-[1.2em] text-gray-700">
+          <div className="flex items-start gap-3 text-[1.2em] text-gray-700"style={{display:'flex',alignItems:'center'}}>
             <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
               <path d="M21 5.5C21 14.0604 14.0604 21 5.5 21C5.11378 21 4.73086 20.9859 4.35172 20.9581C3.91662 20.9262 3.69906 20.9103 3.50103 20.7963C3.33701 20.7019 3.18146 20.5345 3.09925 20.364C3 20.1582 3 19.9181 3 19.438V16.6207C3 16.2169 3 16.015 3.06645 15.842C3.12515 15.6891 3.22049 15.553 3.3441 15.4456C3.48403 15.324 3.67376 15.255 4.05321 15.117L7.26005 13.9509C7.70153 13.7904 7.92227 13.7101 8.1317 13.7237C8.31637 13.7357 8.49408 13.7988 8.64506 13.9058C8.81628 14.0271 8.93713 14.2285 9.17882 14.6314L10 16C12.6499 14.7999 14.7981 12.6489 16 10L14.6314 9.17882C14.2285 8.93713 14.0271 8.81628 13.9058 8.64506C13.7988 8.49408 13.7357 8.31637 13.7237 8.1317C13.7101 7.92227 13.7904 7.70153 13.9509 7.26005L15.117 4.05321" />
             </svg>
-            <p>+593 97 921 2069</p>
+            <p>+593 98 805 3010</p>
+          </div>
+
+          <div className="flex items-start gap-3 text-[1.2em] text-gray-700" style={{display:'flex',alignItems:'center'}}>
+            <svg width={20}height={20}viewBox="0 0 24 24"fill="none">
+  <path d="M4 7.00005L10.2 11.65C11.2667 12.45 12.7333 12.45 13.8 11.65L20 7"stroke="currentColor"strokeWidth={2}strokeLinecap="round"strokeLinejoin="round"/>
+  <rect x="3"y="5"width="18"height="14"rx="2"stroke="currentColor"strokeWidth={2}strokeLinecap="round"/>
+</svg>
+            <p>florindafloreria@gmail.com</p>
           </div>
 
           {/* Social Icons */}
